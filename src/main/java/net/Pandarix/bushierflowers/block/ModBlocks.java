697abd7cf0ca1a6a5cccb0a0933d1429e814a632
Ruven <ruven@uni-bremen.de>
2022-10-01 17:59:20 +0200

    fixed bugs

--- src/main/java/net/Pandarix/bushierflowers/block/ModBlocks.java
+++ src/main/java/net/Pandarix/bushierflowers/block/ModBlocks.java
@@ -5,6 +5,7 @@ import net.Pandarix.bushierflowers.block.custom.GrowablePoppy;
 import net.Pandarix.bushierflowers.item.ModItemGroup;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.item.BlockItem;
--- src/main/java/net/Pandarix/bushierflowers/block/custom/GrowableFlower.java
+++ src/main/java/net/Pandarix/bushierflowers/block/custom/GrowableFlower.java
@@ -1,24 +1,25 @@
 package net.Pandarix.bushierflowers.block.custom;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.ShapeContext;
+import net.minecraft.block.*;
 import net.minecraft.entity.ai.pathing.NavigationType;
+import net.minecraft.tag.BlockTags;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.WorldAccess;
+import net.minecraft.world.WorldView;
 
-public class GrowableFlower extends Block {
+public class GrowableFlower extends PlantBlock {
     /*
     This is the base class of all Growable Flowers in this Mod. This ensures that the Block behaves correctly.
      */
-    public GrowableFlower(Settings settings) {super(settings);}
+    public GrowableFlower(AbstractBlock.Settings settings) {super(settings);}
 
     //---------------------------Block Shape stuff---------------------------
 
     //The blocks outline shape
-    private static final VoxelShape SHAPE = Block.createCuboidShape(0, 0, 0, 16, 6, 16);
-
+    private static final VoxelShape SHAPE = Block.createCuboidShape(2, 0, 2, 14, 8, 14);
     @Deprecated
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
@@ -31,7 +32,4 @@ public class GrowableFlower extends Block {
     //---------------------------Additional settings---------------------------
     @Override
     public boolean canMobSpawnInside() {return true;}
-    @Deprecated
-    @Override
-    public boolean canPathfindThrough(BlockState state, BlockView world, BlockPos pos, NavigationType type) {return true;}
 }
