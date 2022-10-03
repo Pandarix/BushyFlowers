9557ea2cbd20976929c2c26664ffe177d7598f38
Ruven <ruven@uni-bremen.de>
2022-10-03 18:05:26 +0200

    improved spawning

--- src/main/java/net/Pandarix/bushierflowers/world/feature/ModConfiguredFeatures.java
+++ src/main/java/net/Pandarix/bushierflowers/world/feature/ModConfiguredFeatures.java
@@ -9,9 +9,9 @@ import net.minecraft.world.gen.stateprovider.BlockStateProvider;
 public class ModConfiguredFeatures {
 
     //DANDELION
-    public static final RegistryEntry<ConfiguredFeature<RandomPatchFeatureConfig, ?>> GROWN_DANDELION_FLOWER = ConfiguredFeatures.register("grown_dandelion_flower", Feature.FLOWER, ConfiguredFeatures.createRandomPatchFeatureConfig(64, PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK, new SimpleBlockFeatureConfig(BlockStateProvider.of(ModBlocks.GROWN_DANDELION)))));
+    public static final RegistryEntry<ConfiguredFeature<RandomPatchFeatureConfig, ?>> GROWN_DANDELION_FLOWER = ConfiguredFeatures.register("grown_dandelion_flower", Feature.FLOWER, ConfiguredFeatures.createRandomPatchFeatureConfig(2, PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK, new SimpleBlockFeatureConfig(BlockStateProvider.of(ModBlocks.GROWN_DANDELION)))));
     //POPPY
-    public static final RegistryEntry<ConfiguredFeature<RandomPatchFeatureConfig, ?>> GROWN_POPPY_FLOWER = ConfiguredFeatures.register("grown_poppy_flower", Feature.FLOWER, ConfiguredFeatures.createRandomPatchFeatureConfig(64, PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK, new SimpleBlockFeatureConfig(BlockStateProvider.of(ModBlocks.GROWN_POPPY)))));
+    public static final RegistryEntry<ConfiguredFeature<RandomPatchFeatureConfig, ?>> GROWN_POPPY_FLOWER = ConfiguredFeatures.register("grown_poppy_flower", Feature.FLOWER, ConfiguredFeatures.createRandomPatchFeatureConfig(2, PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK, new SimpleBlockFeatureConfig(BlockStateProvider.of(ModBlocks.GROWN_POPPY)))));
 
     public static void registerConfiguredFeatures() {
         System.out.println("Registering ModConfiguredFeatures for " + BushierFlowers.MOD_ID);
